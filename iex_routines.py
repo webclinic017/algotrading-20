76157b6f96dab5756237b22b4ce3e99a0c96bca4
etomasso1 <edwardtomasso@gmail.com>
2021-02-20 10:35:49 -0500

    New daily symbols from iex_routines

--- iex_routines.py
+++ iex_routines.py
@@ -23,9 +23,98 @@ import datetime
 from datetime import date, timedelta, time
 
 try:
-    from dev.help_class import baseDir
+    from dev.help_class import baseDir, dataTypes, getDate
+    from dev.iex_class import readData, urlData
 except ModuleNotFoundError:
-    from help_class import baseDir
+    from help_class import baseDir, dataTypes, getDate
+    importlib.reload(sys.modules['help_class'])
+    from help_class import baseDir, dataTypes, getDate
+
+    from iex_class import readData, urlData
+    importlib.reload(sys.modules['iex_class'])
+    from iex_class import readData, urlData
+
+# Display max 50 columns
+pd.set_option('display.max_columns', None)
+# Display maximum rows
+pd.set_option('display.max_rows', 500)
+
+# %% codecell
+##############################################
+
+# %% codecell
+##############################################
+
+class dailySymbols():
+    """Get a list of all symbols supported by IEX."""
+
+    def __init__(self):
+        self.new_syms = self.get_merge_sort(self)
+        self.new_syms_tp = self.new_syms_ref_type(self)
+        self.write_to_json(self)
+
+    @classmethod
+    def get_merge_sort(cls, self):
+        """Get new symbols and find diff with last date."""
+        current_syms = urlData("/ref-data/iex/symbols").df
+        old_syms = readData.last_bus_day_syms()
+
+        # Get the differennce between todays/yesterdays symbols
+        syms_diff = (pd.concat([current_syms['symbol'], old_syms['symbol']])
+                       .drop_duplicates(keep=False))
+        # New symbols from last time checking symbols (yesterday)
+        new_syms = (current_syms[(current_syms['symbol'].isin(syms_diff)) &
+                                 (current_syms['isEnabled'])])
+        # Ignore cryptocurrency pairs
+        new_syms = new_syms[~new_syms['symbol'].str[-4:].isin(['USDT'])]
+
+        return new_syms
+
+    @classmethod
+    def new_syms_ref_type(cls, self):
+        """Get reference type data for new symbols."""
+        iex_sup = urlData("/ref-data/symbols").df
+        iex_sup.drop(columns=
+                     ['exchangeSuffix', 'exchangeName',
+                      'name', 'iexId', 'region',
+                      'currency', 'isEnabled', 'cik', 'lei', 'figi'],
+                     axis=1, inplace=True)
+        # Convert data types
+        iex_sup = dataTypes(iex_sup).df
+        new_syms_tp = iex_sup[iex_sup['symbol'].isin(self.new_syms['symbol'])]
+        new_syms_tp.reset_index(inplace=True, drop=True)
+        return new_syms_tp
+
+    @classmethod
+    def write_to_json(cls, self):
+        """Write new symbols json file to local json file."""
+        date_to_use = getDate().query('occ')
+        syms_fname = f"{baseDir().path}/tickers/new_symbols/{date_to_use}.gz"
+        self.new_syms_tp.to_json(syms_fname, compression='gzip')
+
+# %% codecell
+##############################################
+
+iex_sup = urlData("/ref-data/symbols").df
+iex_df = iex_sup.copy(deep=True)
+
+iex_df.drop(columns=
+            ['exchangeSuffix', 'exchangeName', 'name', 'iexId', 'region',
+             'currency', 'isEnabled', 'cik', 'lei', 'figi'],
+            axis=1, inplace=True)
+iex_df = dataTypes(iex_df).df
+
+new_syms_tp = iex_df[iex_df['symbol'].isin(new_syms['symbol'])]
+
+
+
+iex_sup.head(10)
+
+old_syms.head(10)
+old_syms.shape
+
+new_syms.shape
+
 
 # %% codecell
 ##############################################
