7d29f004514c638b9d13748484cdc1e88fcfda3d
etomasso1 <edwardtomasso@gmail.com>
2021-05-12 10:10:25 -0400

    scans and cboe_daily_mmo fix

--- openfigi.py
+++ openfigi.py
@@ -1,9 +1,19 @@
 """Workbook for openfigi."""
-
 # %% codecell
 ##################################################
+from datetime import date
+import json
+from io import BytesIO
+import os
 
+import requests
+import pandas as pd
 
+# Display max 50 columns
+pd.set_option('display.max_columns', None)
+
+# Display maximum rows
+pd.set_option('display.max_rows', 500)
 
 # %% codecell
 ##################################################
@@ -41,6 +51,85 @@ post_df
 post_df.head(1)
 post.content
 
+
+# fpath = "/Users/unknown1/Algo/data/sec/inst_holds_com/_comb.gz"
+
+
+# %% codecell
+##################################################
+from dotenv import load_dotenv
+load_dotenv()
+
+figi_api = os.environ.get("openfigi_api")
+
+fpath = "/Users/unknown1/Algo/data/sec/inst_holds_com/_comb.gz"
+comb_holdings_df = pd.read_json(fpath, compression='gzip')
+cusip_list = comb_holdings_df['cusip'].value_counts().index.tolist()
+
+comb_holdings_df.head(10)
+
+headers = {'content-type': 'text/json', 'X-OPENFIGI-APIKEY': figi_api}
+base_url = 'https://api.openfigi.com/v3/mapping'
+url = f"{base_url}"
+
+payload_all = [{'idType': 'ID_CUSIP', 'idValue': val} for val in cusip_list]
+all_df_list = []
+while len(payload_all) != 0:
+    payload = []
+    for n, val in enumerate(payload_all):
+        if n <= 98:
+            payload.append(payload_all[n])
+            payload_all.remove(val)
+        else:
+            break
+
+    if len(payload_all) <= 99:
+        if (len(payload_all) == len(payload)):
+            post = requests.post(url, data=json.dumps(payload), headers=headers)
+            #df = pd.DataFrame(post.json()[0]['data']).copy(deep=True)
+            #df['cusip'] = [payload[num]['idValue'] for num in range(len(payload))]
+            all_df_list.append(pd.DataFrame(post.json()[0]['data']))
+            break
+    else:
+        post = requests.post(url, data=json.dumps(payload), headers=headers)
+        #df = pd.DataFrame(post.json()[0]['data']).copy(deep=True)
+        #df['cusip'] = [payload[num]['idValue'] for num in range(len(payload))]
+        all_df_list.append(pd.DataFrame(post.json()[0]['data']))
+
+
+all_df = pd.concat(all_df_list)
+all_df.shape
+all_df.drop_duplicates(subset='ticker', inplace=True)
+
+all_df['exchCode'].value_counts()
+
+all_df['securityDescription'].value_counts()
+
+comb_holdings_df.head(50)
+
+all_df.head(10)
+
+df['cusip'] = [payload[num]['idValue'] for num in range(len(payload))]
+all_df['ticker']
+
+all_df.head(10)
+
+from api import serverAPI
+all_syms = serverAPI('all_symbols').df
+comb_holdings_df.rename(columns={'nameOfIssuer': 'name'}, inplace=True)
+
+all_syms['name_low'] = all_syms['name'].str.lower()
+comb_holdings_df['name_low'] = comb_holdings_df['name'].str.lower()
+
+comb_test = pd.merge(comb_holdings_df, all_syms, on='name_low', indicator=True)
+comb_test['name_low'].value_counts()
+comb_test.head(50)
+
+comb_holdings_df['name_low'].value_counts().head(50)
+comb_holdings_df.head(10)
+
+all_syms = serverAPI('all_symbols').df
+all_syms.head(5)
 
 # %% codecell
 ##################################################
--- sec_13_.py
+++ sec_13_.py
@@ -156,42 +156,90 @@ ref_df.shape
 inst_holds_df = serverAPI('sec_inst_holdings').df
 
 inst_holds_df['CIK'].value_counts()
-inst_holds_df.head(10)
+
+
 
 # fpath = secFpaths(sym='TDAC', cat='company_idx')
-url = 'https://algotrading.ventures/api/v1/sec/funcs/add_ciks_13fhrs'
-get = requests.get(url)
-
-
-base_dir = baseDir().path
-forms_fpath = f"{base_dir}/sec/institutions/**/*.gz"
-choices = glob.glob(forms_fpath)
-
-for choice in choices:
-    df = pd.read_json(choice, compression='gzip')
-    break
-
-df.head(10)
-# %% codecell
-#####################################################
-
-add_ciks_to_13FHRs()
-base_dir = baseDir().path
-forms_fpath = f"{base_dir}/sec/institutions/**/*.gz"
-import glob
-choices = glob.glob(forms_fpath, recursive=True)
-
-df = None
-for choice in choices:
-    df = pd.read_json(choice, compression='gzip').copy(deep=True)
-    print(choice)
-    break
-
-
-
-
-df.head(10)
-
 
 # %% codecell
 #####################################################
+from datetime import timedelta
+
+# %% codecell
+#####################################################
+
+
+class scansClass():
+    """Class to implement markets scans."""
+
+    base_dir = baseDir().path
+    df, comb_path = None, None
+
+    def __init__(self, which, by):
+        self.determine_params(self, which, by)
+        if not isinstance(self.df, pd.DataFrame):
+            self.call_func(self, which, by)
+
+    @classmethod
+    def determine_params(cls, self, which, by):
+        """Determine parameters used for functions."""
+        dt = getDate.query('iex_close')
+        dt_1, path_1 = dt - timedelta(days=1), False
+
+        fpath_dict = ({
+            'vol': {
+                'avg': f"{self.base_dir}/scans/top_vol/_{dt}.gz",
+                'avg_1': f"{self.base_dir}/scans/top_vol/_{dt_1}.gz"
+            }
+        })
+
+        path = fpath_dict[which][by]
+        # If path is a file, read file and return
+        if os.path.isfile(path):
+            self.df = pd.read_json(path)
+            return
+        else:
+            path_1 = fpath_dict[which][f"{by}_1"]
+            self.get_comb_path(self, path, path_1, dt)
+
+    @classmethod
+    def get_comb_path(cls, self, path, path_1, dt):
+        """Get iex combined path data."""
+        # If one day behind is still not a file path, look for iex_combined
+        if not os.path.isfile(path_1):
+            for n in list(range(1, 6)):
+                dt = dt - timedelta(days=n)
+                comb_path = f"{self.base_dir}/iex_eod_quotes/combined/_{dt}.gz"
+                if os.path.isfile(comb_path):
+                    self.comb_path = comb_path
+                    break
+        # If IEX data from today, then today is the path. If yesterday, then _1
+        if path_1:
+            self.path = path_1
+        else:
+            self.path = path
+
+    @classmethod
+    def call_func(cls, self, which, by):
+        """Determine class function to call."""
+        if which == 'vol' and by == 'avg':
+            self.call_vol_avg(self)
+
+    @classmethod
+    def call_vol_avg(cls, self):
+        """Perform ops for highest volume/avg."""
+        # Read all symbols fpath and filter to only common stock
+        syms_fpath = f"{self.base_dir}/tickers/all_symbols.gz"
+        all_symbols = pd.read_json(syms_fpath, compression='gzip')
+        cs_syms = all_symbols[all_symbols['type'] == 'cs']['symbol'].tolist()
+
+        # Read all iex_close data, sort to only common stock, create new column
+        df = pd.read_json(self.comb_path, compression='gzip')
+        df = df[df['symbol'].isin(cs_syms)].copy(deep=True)
+        df['vol/avg'] = (df['volume'] / df['avgTotalVolume'] * 100).round(0)
+        df['changePercent'] = (df['changePercent'] * 100).round(1)
+        df['ytdChange'] = (df['ytdChange'] * 100).round(1)
+        self.df = df.sort_values(by=['vol/avg'], ascending=False).head(50)
+        # Write to local json file
+
+        self.df.to_json(self.path, compression='gzip')
