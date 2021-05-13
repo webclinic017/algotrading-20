560ef63f2168147a079a42be5e5664c975fa31eb
etomasso1 <edwardtomasso@gmail.com>
2021-05-13 18:46:12 -0400

    top_vol scan fix

--- daily_front_funcs/scans.py
+++ daily_front_funcs/scans.py
@@ -4,6 +4,7 @@
 import os.path
 from datetime import date, timedelta
 import pandas as pd
+from pandas.tseries.offsets import BusinessDay
 
 try:
     from scripts.dev.multiuse.help_class import baseDir, getDate
@@ -24,12 +25,13 @@ class scansClass():
         self.determine_params(self, which, by)
         if not isinstance(self.df, pd.DataFrame):
             self.call_func(self, which, by)
+            self.write_to_json(self)
 
     @classmethod
     def determine_params(cls, self, which, by):
         """Determine parameters used for functions."""
         dt = getDate.query('iex_close')
-        dt_1, path_1 = dt - timedelta(days=1), False
+        dt_1, path_1 = (dt - BusinessDay(n=1)).date(), False
 
         fpath_dict = ({
             'vol': {
@@ -39,32 +41,20 @@ class scansClass():
         })
 
         path = fpath_dict[which][by]
+        comb_base_path = f"{self.base_dir}/iex_eod_quotes/combined"
         # If path is a file, read file and return
         if os.path.isfile(path):
             self.df = pd.read_json(path)
-        else:
-            path_1 = fpath_dict[which][f"{by}_1"]
-            if os.path.isfile(path_1):
-                self.df = pd.read_json(path_1)
-            else:
-                self.get_comb_path(self, path, path_1, dt)
-
-    @classmethod
-    def get_comb_path(cls, self, path, path_1, dt):
-        """Get iex combined path data."""
-        # If one day behind is still not a file path, look for iex_combined
-        if not os.path.isfile(path_1):
-            for n in list(range(1, 6)):
-                dt = dt - timedelta(days=n)
-                comb_path = f"{self.base_dir}/iex_eod_quotes/combined/_{dt}.gz"
-                if os.path.isfile(comb_path):
-                    self.comb_path = comb_path
-                    break
-        # If IEX data from today, then today is the path. If yesterday, then _1
-        if path_1:
-            self.path = path_1
-        else:
-            self.path = path
+        else:  # Look for iex_eod_combined local file path
+            comb_path = f"{comb_base_path}/_{dt}.gz"
+            if os.path.isfile(comb_path):
+                self.comb_path = comb_path
+                self.path = fpath_dict[which][by]
+            else:  # If comb_fpath data isn't available, go one day back
+                comb_path_1 = f"{comb_base_path}/_{dt_1}.gz"
+                if os.path.isfile(comb_path_1):
+                    self.comb_path = comb_path_1
+                    self.path = fpath_dict[which][f"{by}_1"]
 
     @classmethod
     def call_func(cls, self, which, by):
@@ -86,7 +76,12 @@ class scansClass():
         df['vol/avg'] = (df['volume'] / df['avgTotalVolume'] * 100).round(0)
         df['changePercent'] = (df['changePercent'] * 100).round(1)
         df['ytdChange'] = (df['ytdChange'] * 100).round(1)
-        self.df = df.sort_values(by=['vol/avg'], ascending=False).head(50)
-        # Write to local json file
+        vol_df = df.sort_values(by=['vol/avg'], ascending=False).head(50)
+        vol_df.reset_index(drop=True, inplace=True)
 
+        self.df = vol_df
+
+    @classmethod
+    def write_to_json(cls, self):
+        """Write dataframe to local compression json."""
         self.df.to_json(self.path, compression='gzip')
