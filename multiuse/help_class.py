06bfa1f5621e340cc89af6a1b9300352094064d0
etomasso1 <edwardtomasso@gmail.com>
2021-08-30 22:53:08 -0400

    Another fix for getDate

--- multiuse/help_class.py
+++ multiuse/help_class.py
@@ -203,7 +203,7 @@ class getDate():
         if date.today().weekday() in (5, 6):
             weekend = True
         elif ((date.today().weekday() == 0)
-                and (not getDate.time_cutoff(cutoff_hm=16.0))):
+                and (getDate.time_cutoff(cutoff_hm=16.0))):
             weekend = True
 
         if site in ('cboe', 'occ'):
