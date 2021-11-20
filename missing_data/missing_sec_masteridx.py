f29928c5b9aafad19ae6e7f582fe05900e8f40a5
etomasso1 <edwardtomasso@gmail.com>
2021-11-20 15:37:36 -0500

    Fixed secMasterIdx - added get_missing_sec_idx

---
+++ missing_data/missing_sec_masteridx.py
@@ -0,0 +1,42 @@
+"""Get missing sec master idx file."""
+# %% codecell
+from pathlib import Path
+from time import sleep
+import importlib
+import sys
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+
+try:
+    from scripts.dev.data_collect.sec_routines import secMasterIdx
+    from scripts.dev.multiuse.help_class import getDate
+    from scripts.dev.api import serverAPI
+except ModuleNotFoundError:
+    from data_collect.sec_routines import secMasterIdx
+    from api import serverAPI
+    from multiuse.help_class import getDate
+
+# %% codecell
+
+def get_missing_sec_master_idx(sma_df=False):
+    """Get missing sec reference data files."""
+    # sma_df is the master index file of all dates
+    if not isinstance(sma_df, pd.DataFrame):
+        sma_df = serverAPI('sec_master_all').df
+        sma_df['CIK'] = sma_df['CIK'].astype('str').str.zfill(10)
+        sma_df.rename(columns={'CIK': 'cik', 'Company Name': 'name'}, inplace=True)
+        sma_df['date'] = pd.to_datetime(sma_df['Date Filed'], format='%Y%m%d', errors='coerce')
+
+    bus_days = getDate.get_bus_days(this_year=True)
+    dt = getDate.query('iex_eod')
+    bus_days = bus_days[bus_days['date'].dt.date <= dt].copy()
+
+    dts_missing = bus_days[~bus_days['date'].isin(sma_df['date'].unique())].copy()
+    dts_missing['dt_format'] = dts_missing['date'].dt.strftime('%Y%m%d')
+
+    for dt in tqdm(dts_missing['dt_format']):
+        smi = secMasterIdx(hist_date=dt)
+        sleep(.5)
+
+# %% codecell
