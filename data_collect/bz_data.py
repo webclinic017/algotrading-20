92fdd82d6eae558ea48fb485b3b9d4d4afc5204a
etomasso1 <edwardtomasso@gmail.com>
2022-01-26 22:04:55 -0500

    Historical bz_data

--- data_collect/bz_data.py
+++ data_collect/bz_data.py
@@ -14,9 +14,11 @@ opts = FirefoxOptions()
 opts.add_argument("--headless")
 
 try:
-    from scripts.dev.multiuse.help_class import baseDir, getDate, write_to_parquet, help_print_arg
+    from scripts.dev.multiuse.help_class import baseDir, getDate, write_to_parquet, help_print_arg, df_create_bins
+    from scripts.dev.api import serverAPI
 except ModuleNotFoundError:
-    from multiuse.help_class import baseDir, getDate, write_to_parquet, help_print_arg
+    from multiuse.help_class import baseDir, getDate, write_to_parquet, help_print_arg, df_create_bins
+    from api import serverAPI
 
 # %% codecell
 
@@ -108,6 +110,30 @@ class BzRecs():
 # %% codecell
 
 
+def get_hist_bz_ratings(year=False):
+    """Initiate data collection of past analyst ratings."""
+    # Pull analyst ratings (default is this year)
+    recs = serverAPI('analyst_recs_scraped').df
+    ytd_busdays = None
+
+    if not year:  # Assume this year
+        bus_days = getDate.get_bus_days(this_year=True)
+        dt = getDate.query('iex_eod')
+        ytd_busdays = bus_days[bus_days['date'].dt.date <= dt]
+
+    dates_needed = (ytd_busdays[~ytd_busdays['date']
+                    .isin(recs['date'].dt.date.unique())]
+                    .copy())
+
+    dt_w_bins = df_create_bins(dates_needed, bin_size=2)
+    for bin in dt_w_bins['bins'].unique():
+        df_mod = dt_w_bins[dt_w_bins['bins'] == bin].copy()
+        dt_min = df_mod['date'].dt.date.min()
+        dt_max = df_mod['date'].dt.date.max()
+
+        WebScrapeBzRates(dt_min=dt_min, dt_max=dt_max)
+
+
 class WebScrapeBzRates:
     """Webscraping bz analyst ratings."""
 
@@ -118,11 +144,12 @@ class WebScrapeBzRates:
     self.df_recs : dataframe of all analyst recommendations
     """
 
-    def __init__(self, testing=False):
+    def __init__(self, testing=False, dt_min=False, dt_max=False):
         if not testing:
             self._initiate_browser(self)
             self._iterate_through_result(self, self.fdrive)
-            self._parse_df_submit_get_request(self, self.df_resp)
+            (self._parse_df_submit_get_request(self,
+             self.df_resp, dt_min, dt_max))
             self._parse_get_request(self, self.get)
 
     @classmethod
@@ -157,13 +184,19 @@ class WebScrapeBzRates:
         self.df_resp = df_resp
 
     @classmethod
-    def _parse_df_submit_get_request(cls, self, df_resp):
+    def _parse_df_submit_get_request(cls, self, df_resp, dt_min, dt_max):
         """Get required params from dataframe, make new request."""
         url = 'https://api.benzinga.com/api/v2.1/calendar/ratings'
         recs_row = (df_resp[df_resp['url'].str.contains(url)]
                     .reset_index(drop=True))
         headers = dict(recs_row['headers'][0])
         params = recs_row['params'][0]
+
+        # Optional parameters for gathering historical data
+        if dt_min:
+            params['parameters[date_from]'] = dt_min
+        if dt_max:
+            params['parameters[date_to]'] = dt_max
 
         # Get data
         get = requests.get(url, headers=headers, params=params)
