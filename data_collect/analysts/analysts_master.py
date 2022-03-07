ad19f35b620b49c84523135a99d01e54184a3fe5
etomasso1 <edwardtomasso@gmail.com>
2022-03-07 09:04:46 -0500

    Analyst pricing estimates update

--- data_collect/analysts/analysts_master.py
+++ data_collect/analysts/analysts_master.py
@@ -4,30 +4,33 @@
 try:
     from scripts.dev.data_collect.analysts.earnings.scraped_dates import scraped_ee_dates
     from scripts.dev.data_collect.analysts.helpers.paths import AnalystRecsPaths
-    from scripts.dev.data_collect.analysts.prices.bz_data import get_hist_bz_ratings
+    from scripts.dev.data_collect.analysts.prices.bz_data import get_hist_bz_ratings, WebScrapeBzRates
 except ModuleNotFoundError:
     from data_collect.analysts.earnings.scraped_dates import scraped_ee_dates
     from data_collect.analysts.helpers.paths import AnalystRecsPaths
-    from data_collect.analysts.prices.bz_data import get_hist_bz_ratings
+    from data_collect.analysts.prices.bz_data import get_hist_bz_ratings, WebScrapeBzRates
 
 # %% codecell
 
 
-class AnalystsMaster(AnalystRecsPaths):
+class AnalystsMaster(AnalystRecsPaths, WebScrapeBzRates):
     """Master class for analyst functions/ops."""
 
     def __init__(self, earnings=False, prices=False, hist=False, **kwargs):
         AnalystRecsPaths.__init__(self)
 
         if prices:
-            self._get_prices(self, prices, **kwargs)
+            self._get_prices(self, prices, hist, **kwargs)
         elif earnings:
             self._get_earnings(self, earnings, hist, **kwargs)
 
     @classmethod
-    def _get_prices(cls, self, prices, **kwargs):
+    def _get_prices(cls, self, prices, hist, **kwargs):
         """Get analyst prices or run historical function."""
-        get_hist_bz_ratings()
+        if hist:
+            get_hist_bz_ratings()
+        else:
+            WebScrapeBzRates.__init__(self)
 
     @classmethod
     def _get_earnings(cls, self, earnings, hist, **kwargs):
