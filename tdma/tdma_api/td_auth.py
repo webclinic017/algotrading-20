a5e4ceb3f34e6034dad575b057b8b6c53edda670
etomasso1 <edwardtomasso@gmail.com>
2022-03-14 14:02:43 -0500

    TDMA Auth update/fix

--- tdma/tdma_api/td_auth.py
+++ tdma/tdma_api/td_auth.py
@@ -33,13 +33,12 @@ class TD_Auth():
 
         if self.dt_now > self.refresh_expiry:
             self._refresh_access_token(self)
-            self._load_params(self)
+            # self._load_params(self)
 
     @classmethod
     def _load_params(cls, self):
         """Load parameters for api calls."""
         load_dotenv()
-        self.token_endpoint = 'https://api.tdameritrade.com/v1/oauth2/token'
         self.client_full = os.environ.get('tdma_client_id', False)
         self.refresh_token = os.environ.get('tdma_refresh_token', False)
         self.access_token = os.environ.get('tdma_access_token', False)
@@ -55,26 +54,29 @@ class TD_Auth():
     def _refresh_access_token(cls, self):
         """Get a new access token."""
         secrets = get_secret()
+        self.token_endpoint = 'https://api.tdameritrade.com/v1/oauth2/token'
         self.client_full = secrets['tdma_client_id']
         self.refresh_token = secrets['tdma_refresh_token']
-        self.refresh_expiry = self.dt_now + timedelta(minutes=30)
+
+        # Print to console that we're getting a new auth token
+        help_print_arg("TD_AUTH: Getting new access token")
 
         client = OAuth2Session(redirect_uri='https://127.0.0.1')
         tokens = (client.refresh_token(self.token_endpoint,
                   refresh_token=self.refresh_token,
                   client_id=self.client_full))
-        # Print to console that we're getting a new auth token
-        help_print_arg("TD_AUTH: Getting new access token")
-        help_print_arg(f"TDMA refresh expiry: {str(self.refresh_expiry)}")
 
-        os.environ['tdma_access_token'] = tokens['access_token']
-        os.environ['tdma_access_expires_at'] = (str(datetime.fromtimestamp(
-                                                tokens['expires_at'])))
+        self.access_token = tokens['access_token']
+        self.refresh_expiry = (str(datetime.fromtimestamp(
+                                   tokens['expires_at'])))
+        # Print to console the expiry date
+        help_print_arg(f"TDMA refresh expiry: {self.refresh_expiry}")
+
         # Write changes to local .env file
         dotenv_file = dotenv.find_dotenv()
         (dotenv.set_key(dotenv_file, "tdma_access_token",
-                        os.environ["tdma_access_token"]))
+                        self.access_token))
         (dotenv.set_key(dotenv_file, "tdma_access_expires_at",
-                        os.environ["tdma_access_expires_at"]))
+                        self.refresh_expiry))
 
 # %% codecell
